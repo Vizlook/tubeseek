187be024a15a3f4634df98227fc8b7331f1af5f4
watchsky <792992259@qq.com>
2026-01-09 21:15:28 +0800

    update query tips

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const QUERY_TIPS = [
   'What is RAG? Screen text contains "LangChain"',
-  "Trump says \"there's no deal until there's a deal.\"",
+  'Elon Musk says "gothic MAGA"',
   "how to use js async function",
 ];
